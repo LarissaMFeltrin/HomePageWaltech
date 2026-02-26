f6d63714b6c7a77478901df066804950a76c6a76
larissa <lari-feltrin@hotmail.com>
2026-02-26 13:38:37 -0300

    fix: adicionar rotas para sitemap.xml e robots.txt

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::get('/sitemap.xml', function () {
+    return response()->file(public_path('sitemap.xml'), [
+        'Content-Type' => 'application/xml',
+    ]);
+});
+
+Route::get('/robots.txt', function () {
+    return response()->file(public_path('robots.txt'), [
+        'Content-Type' => 'text/plain',
+    ]);
+});
